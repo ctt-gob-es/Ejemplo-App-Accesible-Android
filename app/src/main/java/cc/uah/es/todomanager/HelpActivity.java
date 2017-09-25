c283daeaf2d433de8ee7476847dd965ed61bed23
francisco.estrada <francisco.estrada@edu.uah.es>
2017-09-25 08:58:13 +0200

    Help activity has been added to load some html help files.

---
+++ app/src/main/java/cc/uah/es/todomanager/HelpActivity.java
@@ -0,0 +1,19 @@
+package cc.uah.es.todomanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "file:///android_asset/html/";
+    private static  final String HELP_FILE = "help.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        WebView webView = (WebView) findViewById(R.id.help_view);
+        webView.loadUrl(BASE_URL + HELP_FILE);
+    }
+}
--- app/src/main/java/cc/uah/es/todomanager/TaskListActivity.java
+++ app/src/main/java/cc/uah/es/todomanager/TaskListActivity.java
@@ -307,7 +307,9 @@ TaskList.getInstance().setTask(task);
             case R.id.settings_option: Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
                 return true;
-            case R.id.help_option: return true;
+            case R.id.help_option: Intent helpIntent = new Intent(this, HelpActivity.class);
+                startActivity(helpIntent);
+                return true;
             case R.id.contact_option: return true;
         }
         return super.onOptionsItemSelected(item);
